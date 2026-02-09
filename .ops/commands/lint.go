3f2257c7ea71b3bc1c6cb325d9d282a2d7a5472c
kiloexabyte <vothanhdat278+1@gmail.com>
2026-02-08 19:11:03 -0600

    refactor: update ops lint and test commands
    - lint: add prettier check using npx
    - test: use npx vitest directly with install step

---
+++ .ops/commands/lint.go
@@ -0,0 +1,31 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+
+	"lesiw.io/command"
+	"lesiw.io/command/sys"
+	"lesiw.io/fs"
+)
+
+func (Ops) Lint() error {
+	sh := command.Shell(sys.Machine(), "golangci-lint", "go")
+	ctx := fs.WithWorkDir(context.Background(), ".ops")
+
+	if err := sh.Exec(ctx, "golangci-lint", "run"); err != nil {
+		return fmt.Errorf("golangci-lint: %w", err)
+	}
+
+	if err := sh.Exec(ctx, "go", "fmt"); err != nil {
+		return fmt.Errorf("go fmt: %w", err)
+	}
+
+	sh = command.Shell(sys.Machine(), "npx")
+	ctx = context.Background()
+	if err := sh.Exec(ctx, "npx", "prettier", "--check", "./**/*.{js,jsx,mjs,cjs,ts,tsx,json,vue}"); err != nil {
+		return fmt.Errorf("prettier: %w", err)
+	}
+
+	return nil
+}
---
+++ .ops/commands/test.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"context"
+	"log"
+
+	"lesiw.io/command"
+	"lesiw.io/command/sys"
+)
+
+func (Ops) Test() {
+	ctx := context.Background()
+	sh := command.Shell(sys.Machine(), "npx", "npm", "pnpm")
+
+	npm := useNpmOrPnpm(ctx, sh)
+
+	err := sh.Exec(ctx, npm, "install")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = sh.Exec(ctx, "npx", "vitest", "run", "tests/units")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
